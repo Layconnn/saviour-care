11cf5bd7faa1dc57db150681d1b4e4e98cddbc60
khalil <olamilekanjinadu21@gmail.com>
2023-05-29 10:39:25 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,184 @@
+import React from "react";
+import './App.scss';
+
+function App() {
+
+  return (
+    <>
+      <div className="nav">
+        <div className="nav__pic">
+          <img src="./image/logo.png" />
+          <p>Saviuor Care</p>
+        </div>
+        <div className="nav__options">
+          <h5>Home</h5>
+          <p>Find a doctor</p>
+          <p>Apps</p>
+          <p>Testimonials</p>
+          <p>About us</p>
+        </div>
+      </div>
+  
+      <hr className="first-hr"/>
+    
+      <img src="./image/ele1.png" className="main" />
+      
+      <div className="section1">
+        <div className="section1__content">
+          <h2>Virtual healthcare Saviour for you</h2>
+          <p className="section1__content__cap">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+          <div className="section1__content__btn">Consult today</div>
+        </div>
+        <img className="section1__eye" src="./image/img1.png" />
+      </div>
+  
+      <h5 className="services">Services we Offer</h5>
+
+      <hr className="second-hr"/>
+
+      <p className="lorem">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. </p>
+      
+      <div className="middle-images">
+        <img src="./image/biggy.png" className="middle-images__biggy" />
+        <img src="./image/ele3.png" className="middle-images__ele3" />
+      </div>
+ 
+
+      <div className="section2">
+        <div className="section2__first">
+          <img src="./image/f2.png" />
+          <h3>Search doctor</h3>
+          <p>Choose your doctor from thousands of specialist, general, and trusted hospitals</p>
+        </div>
+        <div className="section2__second">
+          <img src="./image/f3.png" />
+          <h3>Online pharmacy</h3>
+          <p>Buy  your medicines with our mobile application with a simple delivery system</p>
+        </div>
+        <div className="section2__third">
+          <img src="./image/f1.png" />
+          <h3>Consultation</h3>
+          <p>Free consultation with our trusted doctors and get the best recomendations</p>
+        </div>
+        <div className="section2__fourth">
+          <img src="./image/f4.png" />
+          <h3>Details info</h3>
+          <p>Free consultation with our trusted doctors and get the best recomendations</p>
+        </div>
+        <div className="section2__fifth">
+          <img src="./image/f5.png" />
+          <h3>Emergency Care</h3>
+          <p>You can get 24/7 urgent care for yourself or your children and yourlovely family</p>
+        </div>
+        <div className="section2__sixth">
+          <img src="./image/f6.png" />
+          <h3>Tracking</h3>
+          <p>Track and save your medical history and health data </p>
+        </div>
+      </div>
+      
+      <div className="explore">Learn More</div>
+  
+    <div className="section3">
+      <img src="./image/img2.png" className="section3__image" />
+      <div className="section3__content">
+        <p className="section3__content__head">Leading healthcare providers</p>
+        <hr className="section3__content__rule" />
+        <p className="section3__content__last">Trafalgar provides progressive, and affordable healthcare, accessible on mobile and online for everyone. To us, it’s not just work. We take pride in the solutions we deliver</p>
+        <div className="section3__content__btn">Learn more</div>
+      </div>
+    </div>
+
+    <img src="./image/ele4.png" className="dots" />
+    
+
+    <div className="section5">
+      <div className="section5__content">
+        <p  className="section5__content__main">Download our Saviour Care mobile app</p>
+        <hr  className="section5__content__rule"/>
+        <p  className="section5__content__last">Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely</p>
+        <div  className="section5__content__btn">Download&nbsp;&nbsp;&nbsp;<img src="./image/down.png"  /> </div>
+      </div>
+      <img src="./image/img3.png" className="section5__image"/>
+    </div>
+
+    <img src="./image/big.png" className="big"/>
+    
+    <div className="section6">
+      <img src="./image/img4.png" className="section6__image" />
+      <div className="section6__content">
+        <p className="section6__content__main">Find a Doctor near you</p>
+        <hr className="section6__content__rule" />
+        <p className="section6__content__last">Our  app and web portal allows you to find the doctor to your nearest location. </p>
+        <div className="section6__content__btn">Find now</div>
+      </div>
+    </div>
+    
+    <img src="./image/ele3.png" className="elee" />
+    
+
+    <div className="section7">
+      <h3 className="section7__head">What our customers are saying</h3>
+      <hr className="section7__rule" />
+      <div className="section7__content">
+        <div className="section7__content__one">
+          <img className="section7__content__one__image" src="./image/prof.png" />
+          <div className="section7__content__one__ps">
+            <p className="section7__content__one__ps__p1">A K</p>
+            <p className="section7__content__one__ps__p2">Abc ltd</p>
+          </div>
+        </div>
+        <p className="section7__content__P">“Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.” </p>
+      </div>
+    </div>
+
+    <img src="./image/ele3.png" className="elee2" />
+
+    <div className="arrow-nav">
+      <img src="./image/arrow.png" className="arrow-nav__arrow" />
+    </div>
+    <div className="scroll-up">
+      <img src="./image/up.png" className="scroll-up__move" />
+      <img src="./image/ele5.png" className="scroll-up__elm" />
+    </div>
+    <div className="footer">
+      <div className="footer__body">
+        <div className="footer__body__one">
+          <div className="footer__body__one__profile"> 
+            <img src="./image/logo.png" />
+            <p className="footer__body__one__profile__name">Saviour Care</p>
+          </div>
+          <p className="footer__body__one__cap1">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+          <p className="footer__body__one__cap2">©AK PVT LTD 2021. All rights reserved</p>
+        </div>
+        <div className="footer__body__two">
+          <div className="footer__body__two__contents">
+            <h5>Company</h5>
+            <p>About</p>
+            <p>Testimonials</p>
+            <p>Find a doctor</p>
+            <p>Apps</p>
+          </div>
+          <div className="footer__body__two__contents1">
+            <h5>Region</h5>
+            <p>Link</p>
+            <p>Link</p>
+            <p>Link</p>
+            <p>Link</p>
+          </div>
+          <div className="footer__body__two__contents2">
+            <h5>Help</h5>
+            <p>Help center</p>
+            <p>Contact support</p>
+            <p>Instructions</p>
+            <p>How it works</p>
+          </div>
+        </div>
+      </div>
+      <img src="./image/ele3.png" className="footer__last-image" />
+    </div>
+    </>
+  )
+}
+
+export default App
